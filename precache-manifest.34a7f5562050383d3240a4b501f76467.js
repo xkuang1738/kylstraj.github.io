23ec5ef4ecd22bea160151223948f08fe57db51e
Chief <a>
2019-05-19 12:49:15 -0700

    built app, pushing

---
+++ precache-manifest.34a7f5562050383d3240a4b501f76467.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f8464f9e2f574609e82bdc921a37deb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1777c053106f98602a10",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "e61d354bf772fb2aa892",
+    "url": "/static/js/2.fbbdf157.chunk.js"
+  },
+  {
+    "revision": "1777c053106f98602a10",
+    "url": "/static/js/main.3c225718.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cs361/precache-manifest.39510fb69e64aca4fb074c87f61cde75.js"
+  "/precache-manifest.34a7f5562050383d3240a4b501f76467.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cs361/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
